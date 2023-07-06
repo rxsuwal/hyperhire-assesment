a84be09297bd10f0b5fee89a87225a22fd44a13c
work.rsuwal@gmail.com <work.rsuwal@gmail.com>
2023-07-06 22:11:27 +0545

    css fix

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Header from './components/header/Header';
+import Post from './components/post/Post';
+
+function App() {
+  return (
+    <div className="com_container p_detail">
+      <Header title="자유톡" />
+      <Post />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/carousel/Slider.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import './slider.scss'
+export default function SimpleSlider(props) {
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
+    return (
+        <Slider {...settings}>
+            {props.children}
+        </Slider>
+    );
+}
